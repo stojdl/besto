a5f3a0768a3e76870dba40f01182a49d43bcc448
stojdl <you@example.com>
2020-12-21 17:16:31 +0100

    gatsby config styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,12 @@ module.exports = {
         },
       },
     }, 
-    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
     // `gatsby-plugin-offline`,
   ],
 }
